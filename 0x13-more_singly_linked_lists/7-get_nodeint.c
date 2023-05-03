0e462650bf4d22b77bad09cee49de5ce475b56a5
Kinshidori <mounirkandri12@gmail.com>
2023-05-02 17:47:48 -0700

    0x13. C - More singly linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - returns the node at a certain index in the linked list
+ * get_nodeint_at_index - return  node at a particular index in linked list
  * @head: first node in linked list
  * @index: index of node to return
  *
- * Return: pointer to node we are looking, or NULL
+ * Return: pointer to node we are looking for, or NULL
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
